0a9202ed9fca153cc468c85c84bdb7944bb8f3f4
zhangyingtao75144 <125929019@qq.com>
2020-02-10 15:28:38 +0800

    小热点销售端1.7.0

--- pages/waiter/index.js
+++ pages/waiter/index.js
@@ -10,6 +10,7 @@ var oss_url = app.globalData.oss_url;
 var openid;
 var avatarUrl;
 var nickName;
+var page = 1;
 Page({
 
   /**
@@ -43,14 +44,72 @@ Page({
       that.setData({
         comment_list: data.result.datalist,
         comment_total: data.result.total,
-        comment_avg_score: data.result.avg_score
+        comment_avg_score: data.result.avg_score,
+        start_date: data.result.start_date,
+        end_date: data.result.end_date,
       })
     })
 
     
     
   },
+  /**
+   * 更改开始、结束日期
+   */
+  bindDateChange: function (e) {
+    var that = this;
+    var date_type = e.currentTarget.dataset.date_type;
+    if (date_type == 1) {
+      that.setData({
+        start_date: e.detail.value
+      })
+    } else {
+      that.setData({
+        end_date: e.detail.value
+      })
+    }
+  },
+  /**
+   * 查询服务员某个时间段的评分列表
+   */
+  warterScoreList: function (e) {
 
+    var that = this;
+    var start_date = e.detail.value.start_date;
+    var end_date = e.detail.value.end_date;
+    utils.PostRequest(api_v_url + '/comment/commentlist', {
+      openid: openid,
+      start_date: start_date,
+      end_date: end_date,
+      page: 1
+    }, (data, headers, cookies, errMsg, statusCode) => {
+      that.setData({
+        comment_list: data.result.datalist,
+        comment_total: data.result.total,
+        comment_avg_score: data.result.avg_score,
+      })
+    })
+  },
+
+  /**
+   * 服务员详情评分列表分页
+   */
+  loadMore: function (e) {
+    var that = this;
+    page += 1;
+    var start_date = that.data.start_date;
+    var end_date = that.data.end_date;
+    utils.PostRequest(api_v_url + '/comment/commentlist', {
+      openid: openid,
+      start_date: start_date,
+      end_date: end_date,
+      page: page
+    }, (data, headers, cookies, errMsg, statusCode) => {
+      that.setData({
+        comment_list: data.result.datalist
+      })
+    });
+  },
   /**
    * 修改头像
    */
