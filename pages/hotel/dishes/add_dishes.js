8375f030c8a69209e4ad9e42640e117dae0d527a
zhangyingtao75144 <125929019@qq.com>
2020-02-12 18:06:08 +0800

    小热点销售端1.8.0

--- pages/hotel/dishes/add_dishes.js
+++ pages/hotel/dishes/add_dishes.js
@@ -1,4 +1,14 @@
 // pages/hotel/dishes/add_dishes.js
+const app = getApp()
+const mta = require('../../../utils/mta_analysis.js')
+const utils = require('../../../utils/util.js')
+var api_url = app.globalData.api_url;
+var api_v_url = app.globalData.api_v_url;
+var cache_key = app.globalData.cache_key;
+var oss_upload_url = app.globalData.oss_upload_url;
+var oss_url = app.globalData.oss_url;
+var hotel_id;
+var openid;
 Page({
 
   /**
@@ -12,9 +22,91 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    openid = options.openid;
+    hotel_id = options.hotel_id;
 
   },
+  /**
+   * 上传菜品图
+   */
+  uploadDishesPic:function(e){
+    var keys = e.currentTarget.dataset.keys;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        var filename = res.tempFilePaths[0];
 
+        var index1 = filename.lastIndexOf(".");
+        var index2 = filename.length;
+        var timestamp = (new Date()).valueOf();
+
+        var postf = filename.substring(index1, index2);//后缀名\
+        var postf_t = filename.substring(index1, index2);//后缀名
+        var postf_w = filename.substring(index1 + 1, index2);//后缀名
+
+        var img_url = timestamp + postf;
+
+        wx.request({
+          url: api_url + '/Smallapp/Index/getOssParams',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          success: function (rest) {
+            var policy = rest.data.policy;
+            var signature = rest.data.signature;
+
+            wx.uploadFile({
+              url: oss_upload_url,
+              filePath: filename,
+              name: 'file',
+              header: {
+                'Content-Type': 'image/' + postf_w
+              },
+              formData: {
+                Bucket: "redian-produce",
+                name: img_url,
+                key: "forscreen/resource/" + img_url,
+                policy: policy,
+                OSSAccessKeyId: app.globalData.oss_access_key_id,
+                sucess_action_status: "200",
+                signature: signature
+
+              },
+
+              success: function (res) {
+
+                
+              },
+              fail: function ({ errMsg }) {
+                
+              },
+            });
+          }
+        })
+      }
+    })
+  },
+  addDishes:function(e){
+    var name = e.detail.value.name;
+    var price = e.detail.value.price;
+    var desc = e.detail.value.desc;
+    var dishes_pic = e.detail.dishes_pic;
+    var desc_img = e.detail.value.desc_img;
+
+    utils.PostRequest(api_v_url + '/aa/bb', {
+      openid: openid,
+      hotel_id:hotel_id,
+      name:name,
+      price:price,
+      desc:desc,
+      desc_img:desc_img
+    }, (data, headers, cookies, errMsg, statusCode) => {
+      app.showToast('添加成功')
+    })
+
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/hotel/order/index.js
+++ pages/hotel/order/index.js
@@ -1,20 +1,112 @@
 // pages/hotel/order/index.js
+const app = getApp()
+const mta = require('../../../utils/mta_analysis.js')
+const utils = require('../../../utils/util.js')
+var api_url = app.globalData.api_url;
+var api_v_url = app.globalData.api_v_url;
+var cache_key = app.globalData.cache_key;
+var hotel_id;
+var openid;
+var page = 1;
+var page_all = 1;
+var page_dealing =1 ;
+var page_complete=1;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    order_status:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    openid = options.openid
+    hotel_id = options.hotel_id;
+
+    //全部订单
+    utils.PostRequest(api_v_url + '/aa/bb', {
+      openid: openid,
+      hotel_id: hotel_id,
+      page :1,
+      order_status:0
+    }, (data, headers, cookies, errMsg, statusCode) => {
+      that.setData({
+        all_order_list:data.result
+      })
+    })
+    //处理中的订单
+    utils.PostRequest(api_v_url + '/aa/bb', {
+      openid: openid,
+      hotel_id: hotel_id,
+      page: 1,
+      order_status: 1
+    }, (data, headers, cookies, errMsg, statusCode) => {
+      that.setData({
+        deal_order_list: data.result
+      })
+    })
+    //已完成的订单
+    utils.PostRequest(api_v_url + '/aa/bb', {
+      openid: openid,
+      hotel_id: hotel_id,
+      page: 1,
+      order_status: 2
+    }, (data, headers, cookies, errMsg, statusCode) => {
+      that.setData({
+        complete_order_list: data.result
+      })
+    })
 
   },
+  swichOrderList:function(e){
+    var that = this;
+    var order_status = e.currentTarget.dataset.order_status
+    that.setData({
+      order_status:order_status,
+    })
 
+  },
+  loadMore:function(e){
+    var that = this;
+    var order_status = that.data.order_status
+    if(order_status==0){
+      page_all +1;
+      page = page_all;
+    }else if(order_status==1){
+      page_dealing +=1;
+      page = page_dealing;
+    }else if (order_status==2){
+      page_complete +=1;
+      page = page_complete;
+    }
+    //已完成的订单
+    utils.PostRequest(api_v_url + '/aa/bb', {
+      openid: openid,
+      hotel_id: hotel_id,
+      page: page,
+      order_status: order_status
+    }, (data, headers, cookies, errMsg, statusCode) => {
+      if(order_status==0){
+        that.setData({
+          all_order_list: data.result
+        })
+      }else if(order_status==1){
+        that.setData({
+          deal_order_list: data.result
+        })
+      }else if(order_status==3){
+        that.setData({
+          complete_order_list:data.result
+        })
+      }
+      
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
