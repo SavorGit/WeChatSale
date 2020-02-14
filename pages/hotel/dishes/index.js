cb003ed593d4ff249d1f341b614a7bda17a9d4ec
zhangyingtao75144 <125929019@qq.com>
2020-02-14 18:48:54 +0800

    小热点销售端1.8.0

--- pages/hotel/dishes/index.js
+++ pages/hotel/dishes/index.js
@@ -129,11 +129,22 @@ Page({
             goods_id: goods_id,
             status:status
           }, (data, headers, cookies, errMsg, statusCode) => {
-            var dishes_list = that.data.dishes_list;
+            /*var dishes_list = that.data.dishes_list;
             dishes_list[keys].status = status;
             that.setData({
               dishes_list: dishes_list
+            })*/
+            utils.PostRequest(api_v_url + '/dish/goodslist', {
+              openid: openid,
+              merchant_id: merchant_id,
+              page: page,
+              hotel_id: hotel_id
+            }, (data, headers, cookies, errMsg, statusCode) => {
+              that.setData({
+                dishes_list: data.result
+              })
             })
+
             app.showToast(toast)
           })
         }
