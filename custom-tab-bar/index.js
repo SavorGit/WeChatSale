6182bc1bb8326e71252c03b42cc78953e1afa6df
lz958942 <404644381@qq.com>
2020-08-18 15:24:17 +0800

    去掉日志输出

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -58,7 +58,7 @@ Component({
    */
   methods: {
     switchTab(e) {
-      console.log(e);
+      //console.log(e);
       const data = e.currentTarget.dataset
       const url = data.path
       wx.switchTab({url});
