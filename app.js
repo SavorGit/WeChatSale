bb7a1fd47ca32aa9cfac6e6f20d99c4124412073
zhangyingtao75144 <125929019@qq.com>
2021-06-18 17:04:22 +0800

    销售端1.9.16

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ App({
     appKey:'60bd92784d0228352bbe1c8b',//由友盟分配的APP_KEY
     useOpenid:true,// 是否使用openid进行统计，此项为false时将使用友盟+随机ID进行用户统计。使用openid来统计微信小程序的用户，会使统计的指标更为准确，对系统准确性要求高的应用推荐使用OpenID。
     autoGetOpenid:true,// 是否需要通过友盟后台获取openid，如若需要，请到友盟后台设置appId及secret
-    debug:true,//是否打开调试模式
+    debug:false,//是否打开调试模式
     uploadUserInfo:false,// 上传用户信息，上传后可以查看有头像的用户分享信息，同时在查看用户画像时，公域画像的准确性会提升。
     enableVerify:true
   },
