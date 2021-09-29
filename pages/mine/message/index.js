e250597fa0ee4892321d5dc615253b7da46dfea1
lz958942 <404644381@qq.com>
2021-09-29 12:21:17 +0800

    增加消息通知首页
    https://modao.cc/app/82ad1a837572df1a214f2c1dc78ec789f716df26?simulator_type=device&sticky#screen=sku4x9svwyqozw3

--- pages/mine/message/index.js
+++ pages/mine/message/index.js
@@ -1,18 +1,41 @@
 // pages/mine/message/index.js
+/**
+ * 消息通知首页
+ */
+
+const app = getApp()
+const utils = require('../../../utils/util.js')
+var api_url = app.globalData.api_url;
+var api_v_url = app.globalData.api_v_url;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    statusBarHeight: getApp().globalData.statusBarHeight,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let self = this;
+    self.setData({
+      list: [{
+        title: '品鉴酒领取通知',
+        time: '2021.9.29 11:30',
+        user_name: '张三三',
+        user_header: 'https://oss.littlehotspot.com/WeChat/resource/default.jpg',
+        msg: '成功领取了品鉴酒“xxxxxxxxxxxxxxxxxx“，请及时处理。'
+      }, {
+        title: '品鉴酒领取通知',
+        time: '2021.9.29  11:20',
+        user_name: '李四四',
+        user_header: 'https://oss.littlehotspot.com/WeChat/resource/default.jpg',
+        msg: '成功领取了品鉴酒“xxxxxxxxxxxxxxxxxx“，请及时处理。'
+      }]
+    });
   },
 
   /**
