ce46f4e672a0871cfbf222767e4d8cfb8cd4c821
lz958942 <404644381@qq.com>
2022-02-22 16:51:13 +0800

    1.【销售端-我的】，增加“幸运抽奖“入口； 2.【销售端】通用码版，增加“幸运抽奖“列表页， 6.【销售端-幸运抽奖列表】中显示当前酒楼配置的幸运抽奖活动； https://modao.cc/app/LRMgYJXrr7n41xN45v6RUI#screen=skzxgtlsillgp1j https://modao.cc/app/LRMgYJXrr7n41xN45v6RUI#screen=skzxgtlsipulpus https://modao.cc/app/LRMgYJXrr7n41xN45v6RUI#screen=skzxgy2ms4xgebv https://modao.cc/app/LRMgYJXrr7n41xN45v6RUI#screen=skzxh4m5wu1j12x

--- pages/mine/turn_lottery/list.js
+++ pages/mine/turn_lottery/list.js
@@ -1,11 +1,22 @@
 // pages/mine/turn_lottery/list.js
+/**
+ * 幸运抽奖页面
+ */
+
+const app = getApp()
+const utils = require('../../../utils/util.js')
+var api_url = app.globalData.api_url;
+var api_v_url = app.globalData.api_v_url;
+var cache_key = app.globalData.cache_key;
+var openid;
+var merchant_id;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    list: [{ name: 'xxxx酒xxxxx抽奖' }, { name: '任务中心' }, { name: '幸运抽奖' }, { name: '餐厅评价' }]
   },
 
   /**
@@ -62,5 +73,17 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  // 设置发起抽奖
+  setPrize: function (e) {
+    let self = this;
+    let index = e.currentTarget.dataset.index;
+    let bean = self.data.list[index];
+    self.setData({ showSetPrizeWindow: true, setPrize: bean });
+  },
+  // 关闭设置奖项弹窗
+  closeSetPrizeWindow: function (e) {
+    let self = this;
+    self.setData({ showSetPrizeWindow: false });
   }
 })
