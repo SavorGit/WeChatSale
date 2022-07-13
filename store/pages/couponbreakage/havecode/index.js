af226840d46f250370331672238389d6aba5a7f7
lz958942 <404644381@qq.com>
2022-07-13 15:37:08 +0800

    增加优惠券核销页面 https://modao.cc/app/FFAXdvyrdk2f077WdDdFq#screen=sl4m4cb2ajf89g2

--- store/pages/couponbreakage/havecode/index.js
+++ store/pages/couponbreakage/havecode/index.js
@@ -1,11 +1,42 @@
 // store/pages/couponbreakage/havecode/index.js
+
+/**
+ * 新增核销申请-优惠券
+ */
+const app = getApp()
+const utils = require('../../../../utils/util.js')
+var uma = app.globalData.uma;
+var api_url = app.globalData.api_url;
+var api_v_url = app.globalData.api_v_url;
+var cache_key = app.globalData.cache_key;
+var openid;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    listTitle: '已扫优惠券（2）',
+    scanList: [{
+      idcode: 'aaaaaaaaaaaaaaaaaaaaaaaaa',
+      add_time: '2022/04/10 11:00:50'
+    }, {
+      idcode: 'bbbbbbbbbbbbbbbbbbbbb',
+      add_time: '2022/04/10 11:00:40'
+    }],
+    datas: [{
+      img_url: '',
+      is_required: 1,
+      name: '酒水小票'
+    }, {
+      img_url: '',
+      is_required: 1,
+      name: '瓶盖照片'
+    }, {
+      img_url: '',
+      is_required: 1,
+      name: '其他'
+    }]
   },
 
   /**
