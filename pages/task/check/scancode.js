69eb372e50ae1ff4d47758b919570275e42a63ce
lz958942 <404644381@qq.com>
2023-06-28 17:04:48 +0800

    https://modao.cc/app/xRyc3w3grwhqazZD7gakhQ#screen=slac7gnf10elj2kmxpsm7gdei https://modao.cc/app/xRyc3w3grwhqazZD7gakhQ#screen=slj2l4f8iney6lb

--- pages/task/check/scancode.js
+++ pages/task/check/scancode.js
@@ -1,4 +1,17 @@
 // pages/task/check/scancode.js
+const utils = require('../../../utils/util.js');
+
+/**
+ * 盘点任务
+ */
+const app = getApp()
+var uma = app.globalData.uma;
+var api_url = app.globalData.api_url;
+var api_v_url = app.globalData.api_v_url;
+var cache_key = app.globalData.cache_key;
+var openid;
+var hotel_id;
+var code_msg;
 Page({
 
   /**
