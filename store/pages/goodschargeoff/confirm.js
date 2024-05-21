7125e3fa8275a1c4b6d98e6e4afc057d85f638b9
lz958942 <404644381@qq.com>
2024-05-21 15:20:50 +0800

    增加确认销售记录 https://modao.cc/app/dhMqWTpPsd78xwrJ2zI7MY#screen=slw5qxjoa2tgtne

--- store/pages/goodschargeoff/confirm.js
+++ store/pages/goodschargeoff/confirm.js
@@ -1,66 +1,79 @@
 // store/pages/goodschargeoff/confirm.js
+const utils = require('../../../utils/util.js');
+
+/**
+ * 核销 新增核销申请
+ */
+const app = getApp()
+var uma = app.globalData.uma;
+var api_url = app.globalData.api_url;
+var api_v_url = app.globalData.api_v_url;
+var cache_key = app.globalData.cache_key;
+var oss_upload_url = app.globalData.oss_upload_url;
+var oss_url = app.globalData.oss_url;
+var openid;
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 确认核销记录页面
+     */
+    data: {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
 
-  }
+    }
 })
